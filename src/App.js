4faac7cb9516cfd839aed29f1d11ed685fb8e3a3
hazedrops <hazedrops@hotmail.com>
2021-11-06 22:13:00 -0700

    React - Anime Search with API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Header from "./components/Header";
+import MainContent from './components/MainContent';
+import Sidebar from './components/Sidebar';
 
 function App() {
+  const [animeList, setAnimeList] = useState([]);
+  const [topAnime, setTopAnime] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const getTopAnime = async () => {
+    const temp = await fetch(`https://api.jikan.moe/v3/top/anime/1/bypopularity`)
+      .then(res => res.json());
+    
+    setTopAnime(temp.top.slice(0,5));
+  }
+
+  useEffect(() => {
+    getTopAnime();
+  }, [])
+
+  const HandleSearch = (e) => {
+    e.preventDefault();
+
+    FetchAnime(search);
+  }
+
+  const FetchAnime = async (query) => {
+    const temp = await fetch(`https://api.jikan.moe/v3/search/anime?q=${query}&order_by=title&sort=asc&limit=10`)
+      .then(res => res.json());
+
+    setAnimeList(temp.results);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="content-wrap">
+        <Sidebar topAnime={topAnime} />
+        <MainContent 
+          HandleSearch={HandleSearch}
+          search={search}
+          setSearch={setSearch}
+          animeList={animeList} />        
+      </div>
     </div>
   );
 }
